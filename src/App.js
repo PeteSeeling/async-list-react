b96db5b20fb62089cb4029700597321e0f6cb64a
Pete Seeling <peteseeling@gmail.com>
2022-02-03 14:34:28 -0600

    client error sorted out. work on app. js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@
 import './App.css';
 import { useState } from 'react';
 
+import AnimalsList from './animals';
+import { getAnimals } from './getAnimals';
+
 
 
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const [isLoading, setLoading] = useState(false);
 
 
-  
+  async function fetchData() {
+    setLoading(true);
+    const data = await getAnimals();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={fetchData}>Fetch Data</button>
+        <h3>Animals List</h3>
+        <AnimalsList animals={animals} />
     
       </header>
     </div>
