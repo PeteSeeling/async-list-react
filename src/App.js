241ce04ae0fc672f33d393411980498ab62e6002
Pete Seeling <peteseeling@gmail.com>
2022-02-03 18:31:22 -0600

    1st list rendering correctly.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import AnimalsList from './animals';
+
+
 import { getAnimals } from './getAnimals';
 
 
 
 
+
+
 function App() {
+ 
   const [animals, setAnimals] = useState([]);
 
   const [isLoading, setLoading] = useState(false);
 
+  const [isAnimalsLoading, setAnimalsLoading] = useState(false);
 
-  async function fetchData() {
-    setLoading(true);
+  async function fetchData(){
+    setAnimalsLoading(true);
+
     const data = await getAnimals();
+    setAnimalsLoading(false);
+    setAnimals(data);
   }
 
+
+  async function fetchAndStoreAnimals() {
+   
+    setAnimalsLoading(true);
+
+    const data = await getAnimals();
+
+    setLoading(false);
+    setAnimals(data);
+ 
+  }
+  console.log(animals);
+  useEffect(() =>{
+    fetchAndStoreAnimals();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={fetchData}>Fetch Data</button>
-        <h3>Animals List</h3>
-        <AnimalsList animals={animals} />
     
-      </header>
+    <div className="App">
+    
+      <button onClick={fetchData}>Fetch Data</button> 
+      <AnimalsList animals= {animals} />
     </div>
+  
+  
   );
 }
 
---
+++ src/animalItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function AnimalItem({ animal, size, country, color }){
+  console.log(animal);
+  return <div className='animal-item'>
+    <div>The {animal} is {size} sized. Its country of origin is  {country} and its color is {color}</div>
+  </div>;
+}
