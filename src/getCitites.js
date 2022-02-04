0e0d06206b07967d1f053c37e583b3c3c402e657
Pete Seeling <peteseeling@gmail.com>
2022-02-03 19:16:08 -0600

    citites item done

--- src/getCitites.js
+++ src/getCitites.js
@@ -1,9 +1,9 @@
 import { checkError, client } from './client';
 
-export async function getCities(){
+export async function getCountries(){
 
   const response = await client
-    .from ('cities')
+    .from ('countries')
     .select();
 
   return checkError(response);
