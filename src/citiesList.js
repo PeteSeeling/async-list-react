e54c05000e00f8a10a16e7fb138363d0b777ee11
Pete Seeling <peteseeling@gmail.com>
2022-02-04 14:03:11 -0600

    console. log deleted

--- src/citiesList.js
+++ src/citiesList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CountriesItem from './citiesItem';
 
 export default function CountriesList({ countries }) {
-  console.log(countries);
+
   return <div className={'countries-list'}>
       Countries List Here:
     <div>{countries.map((country, i) => <CountriesItem key ={country, i} country={country.country} population={country.population} visited={country.visited} language={country.language} />)}
