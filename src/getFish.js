b8d8ab88a138569a95029355da118103a690188c
Pete Seeling <peteseeling@gmail.com>
2022-02-03 19:29:52 -0600

    4th list files created.

---
+++ src/getFish.js
@@ -0,0 +1,11 @@
+import { checkError, client } from './client';
+
+export async function getCountries(){
+
+  const response = await client
+    .from ('countries')
+    .select();
+
+  return checkError(response);
+      
+}
